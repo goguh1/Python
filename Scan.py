f1ac991b34223383fd79572208cb95b7980919c9
Hugo G <92497615+goguh1@users.noreply.github.com>
2023-12-24 18:42:13 -0800

    Create Scan.py

---
+++ Scan.py
@@ -0,0 +1,26 @@
+# Create a folder on your computer called "week3"
+# Create a script that, when run,checks every second indefinitely for a new file in that folder
+# If file found,report back to the user:
+# 1.File found
+# 2.What type of file it is
+# 3.When it was put there
+
+# You as the user can introduce whatever file you want to invoke the notification portion of the script (i.e put a text file, image, etc)
+
+import os
+from datetime import datetime
+
+# Directory to be scanned
+f1 = '/Users/hugog/Documents/Python/Week3'
+
+obj = os.scandir(f1)
+
+# List all files and directories in the specified path
+print("Files found in '%s':" % f1)
+for entry in obj:
+    if entry.is_dir() or entry.is_file():
+        name = entry.name
+        ctime = datetime.fromtimestamp(entry.stat().st_ctime)
+        print(f"{name} - Created on: {ctime.strftime('%Y-%m-%d %H:%M:%S')}")
+
+obj.close()
